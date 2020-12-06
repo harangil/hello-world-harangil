b5e3fd27c65e2240ea38eb23de599427b38b1412
harangil <73331396+harangil@users.noreply.github.com>
2020-12-06 19:37:47 +0200

    CV Site

---
+++ cv_haran_gilboa/ex6.js
@@ -0,0 +1,10 @@
+function change_color(col){
+    col.style.backgroundColor= "hotpink";
+}
+function change_back(col){
+    col.style.backgroundColor= "white";
+}
+function say_thank_you(x){
+    alert("Thank you! I will contact as soon as possible!");
+    document.getElementById("myform").innerHTML = "Request submitted" ;
+}
